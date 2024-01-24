2d0ff1c73cb502556a8bcac1a8317e71438b7293
Toandone <toanptde160263@fpt.edu.vn>
2024-01-24 18:06:52 +0700

    feat(function): add required controller

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,30 @@
+﻿using MedRoute.Models;
+using MedRoute.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedRoute.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IBookingRepository _bookingRepository;
+
+        public BookingController(IBookingRepository bookingRepository)
+        {
+            _bookingRepository = bookingRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Book()
+        {
+            return View();
+        }
+        public IActionResult Book([Bind()] MedicalRecord record)
+        {
+            return View();
+        }
+    }
+}
