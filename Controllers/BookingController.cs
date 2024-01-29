0b59cebf5d83c4e8b1f3cd7c3f012406c607202e
Nguyen Anh Viet <van123872000@gmail.com>
2024-01-29 19:39:52 +0700

    feat(function): add update medical record

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -6,6 +6,7 @@ using MedRoute.Repository.Implement;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using MedRoute.Services;
 
 namespace MedRoute.Controllers
 {
@@ -32,18 +33,52 @@ namespace MedRoute.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //var userId = Convert.ToInt32(User.FindFirstValue("Id"));
-            Booking booking = null;
-            var bookingRs = await ((BookingRepository)_bookingRepository).GetAllAsync();
-            if (bookingRs.IsSuccess)
+            try
             {
-                booking = (bookingRs.Data as DbSet<Booking>).ToList().
-                    OrderByDescending(b => b.Date).
-                    FirstOrDefault();
-
+                // set cookie
+                var cookieHandler = new CookieService();
+                // get init value
+                var userId = Convert.ToInt32(User.FindFirstValue("Id"));
+                var medicalRecordId = Convert.ToInt32(cookieHandler.GetCookie(HttpContext, "MedicalRecordId"));
+                // declare
+                Booking booking = null;
+                User user = null;
+                MedicalRecord medicalRecord = null;
+                // get booking information      
+                var bookingRs = await ((BookingRepository)_bookingRepository).GetAllAsync();
+                if (bookingRs.IsSuccess)
+                {
+                    booking = (bookingRs.Data as DbSet<Booking>).ToList().
+                        OrderByDescending(b => b.Date).
+                        FirstOrDefault();
+                }
+                else throw new Exception(bookingRs.Message);
+                // get insurrance if it have
+                if (medicalRecordId > 0)
+                {
+                    var medicalRecordRs = await ((MedicalRecordRepository)_medicalRecordRepository).GetByIdAsync(medicalRecordId);
+                    if (medicalRecordRs.IsSuccess)
+                    {
+                        medicalRecord = medicalRecordRs.Data as MedicalRecord;
+                        userId = (userId > 0 )? userId : medicalRecord.PatientId.GetValueOrDefault();
+                    }
+                    else throw new Exception(medicalRecordRs.Message);
+                }
+                // get user information
+                var userRs = await ((UserRepository)_userRepository).GetByIdAsync(userId);
+                if (userRs.IsSuccess)
+                    user = userRs.Data as User;
+                else throw new Exception(userRs.Message);
+                // sending data
+                ViewBag.LatestBooking = booking;
+                ViewBag.User = user;
+                ViewBag.MedicalRecord = medicalRecord;
             }
-            ViewBag.LatestBooking = booking;
-
+            catch (Exception ex)
+            {
+                TempData["isSucces"] = false;
+                TempData["ErrMess"] = ex.Message;
+            }
             return View();
         }
 
@@ -52,48 +87,80 @@ namespace MedRoute.Controllers
 
             return View();
         }
+
+        private async Task<MedicalRecord> CreateAMedicalRecord(BookingForm bookingForm, 
+            Booking booking, User user)
+        {
+            MedicalRecord medicalRecord = null;
+            var medicalRs = await ((MedicalRecordRepository)_medicalRecordRepository).
+                    CreateAsync(new MedicalRecord
+                    {
+                        MedicalDetail = bookingForm.MedicalDetail,
+                        MedicalResult = "",
+                        PatientId = user.UserId,
+                        BookingId = bookingForm.BookingId,
+                        BookingOrder = booking.Order,
+                        Status = ConstVariable.MEDICAL_RECORD_STATUS_WAITING
+                    });
+            if (medicalRs.IsSuccess)
+                medicalRecord = (medicalRs.Data as MedicalRecord);
+            else throw new Exception(medicalRs.Message);
+            return medicalRecord;
+        }
+        private async Task<MedicalRecord> UpdateAMedicalRecord(MedicalRecord medicalRecord)
+        {
+            // 
+            var medicalDetail = medicalRecord.MedicalDetail;
+            // get record
+            var medicalRs = await ((MedicalRecordRepository)_medicalRecordRepository).
+                    GetByIdAsync(medicalRecord.MedicalRecordId);
+            if (medicalRs.IsSuccess)
+                medicalRecord = (medicalRs.Data as MedicalRecord);
+            else throw new Exception(medicalRs.Message);
+            // update 
+            medicalRecord.MedicalDetail = medicalDetail;
+            medicalRs = await ((MedicalRecordRepository)_medicalRecordRepository).
+                    UpdateAsync(medicalRecord);
+            if (medicalRs.IsSuccess)
+                medicalRecord = (medicalRs.Data as MedicalRecord);
+            else throw new Exception(medicalRs.Message);
+            return medicalRecord;
+        }
+
         [HttpPost]
-        public async Task<IActionResult> Book([Bind()] BookingForm bookingForm)
+        public async Task<IActionResult> BookUser([Bind()] BookingForm bookingForm)
         {
             //var u = bookingForm.user.FullName;
             try
             {
+                // set cookie
+                var cookieHandler = new CookieService();
                 // declare
                 Booking booking = null;
                 User user = null;
                 MedicalRecord medicalRecord = null;
-                // get userid
+                // get 
                 var userId = Convert.ToInt32(User.FindFirstValue("Id"));
-                // create a guest
-                if(userId == null || userId == 0)
-                {
-                    // case user loged in
-                    var userRs = await ((UserRepository)_userRepository).
-                    CreateAsync(new Models.User
-                    {
-                        UserName = "",
-                        FullName = bookingForm.FullName,
-                        DateOfBirth = bookingForm.DateOfBirth,
-                        Gender = bookingForm.Gender,
-                        PhoneNumber = bookingForm.PhoneNumber,
-                        Email = bookingForm.Email,
-                        InsuranceCode = bookingForm.InsuranceCode,
-                        RoleId = 1
-                    });
-                    if (userRs.IsSuccess)
-                        user = (userRs.Data as User);
-                    else throw new Exception(userRs.Message);
-                }
-                else
-                {
-                    // case user do not log in
-                    var userRs = await ((UserRepository)_userRepository).
-                    GetByIdAsync(userId);
-                    if (userRs.IsSuccess)
-                        user = (userRs.Data as User);
-                    else throw new Exception(userRs.Message);
-                }
-                
+                var medicalRecordId = Convert.ToInt32(cookieHandler.GetCookie(HttpContext, "MedicalRecordId"));
+                // get a customer
+                var userRs = await ((UserRepository)_userRepository).
+                GetByIdAsync(userId);
+                if (userRs.IsSuccess)
+                    user = (userRs.Data as User);
+                else throw new Exception(userRs.Message);
+                // set user information 
+                user.FullName = bookingForm.FullName;
+                user.DateOfBirth = bookingForm.DateOfBirth;
+                user.Gender = bookingForm.Gender;
+                user.PhoneNumber = bookingForm.PhoneNumber;
+                user.Email = bookingForm.Email;
+                user.InsuranceCode = bookingForm.InsuranceCode;
+                // update user
+                userRs = await ((UserRepository)_userRepository).
+                UpdateAsync(user);
+                if (userRs.IsSuccess)
+                    user = (userRs.Data as User);
+                else throw new Exception(userRs.Message);
 
                 // get bookingid and bookingorder
                 var bookingRs = await ((BookingRepository)_bookingRepository).GetByIdAsync(bookingForm.BookingId);
@@ -102,30 +169,141 @@ namespace MedRoute.Controllers
                 else throw new Exception(bookingRs.Message);
 
                 // create a insurrance and save it in cookie
-                var medicalRs = await ((MedicalRecordRepository)_medicalRecordRepository).
-                    CreateAsync(new MedicalRecord
+                if (medicalRecordId > 0)
+                {
+                    medicalRecord = await UpdateAMedicalRecord(new MedicalRecord()
                     {
-                        MedicalDetail = bookingForm.MedicalDetail,
-                        MedicalResult= "",
-                        PatientId = user.UserId,
-                        BookingId = bookingForm.BookingId,
-                        BookingOrder = booking.Order,
-                        Status = ConstVariable.MEDICAL_RECORD_STATUS_WAITING
-                    }) ;
-
-                // increase
-                booking.Order = booking.Order + 1;
-                bookingRs = await ((BookingRepository)_bookingRepository).UpdateAsync(booking);
-                if (!bookingRs.IsSuccess)
-                    throw new Exception(bookingRs.Message);
+                        MedicalRecordId = medicalRecordId,
+                        MedicalDetail = bookingForm.MedicalDetail
+                    });
+                }
+                else
+                {
+                    medicalRecord = await CreateAMedicalRecord(bookingForm, booking, user);
+                    // increase
+                    booking.Order = booking.Order + 1;
+                    bookingRs = await ((BookingRepository)_bookingRepository).UpdateAsync(booking);
+                    if (!bookingRs.IsSuccess)
+                        throw new Exception(bookingRs.Message);
+                }
+                
+               
                 // booking successfull
                 TempData["isSucces"] = true;
+                // set cookie
+                cookieHandler.AddCookie(HttpContext, 1, "MedicalRecordId", medicalRecord.MedicalRecordId.ToString());
             }
             catch (Exception ex)
             {
                 TempData["isSucces"] = false;
                 TempData["ErrMess"] = ex.Message;
             }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BookGuest([Bind()] BookingForm bookingForm)
+        {
+            //var u = bookingForm.user.FullName;
+            try
+            {
+                // set cookie
+                var cookieHandler = new CookieService();
+                // declare
+                Booking booking = null;
+                User user = null;
+                MedicalRecord medicalRecord = null;
+                // get 
+                var userId = Convert.ToInt32(User.FindFirstValue("Id"));
+                var medicalRecordId = Convert.ToInt32(cookieHandler.GetCookie(HttpContext, "MedicalRecordId"));
+                // create a guest
+                var userRs = await ((UserRepository)_userRepository).
+                CreateAsync(new Models.User
+                {
+                    UserName = "",
+                    FullName = bookingForm.FullName,
+                    DateOfBirth = bookingForm.DateOfBirth,
+                    Gender = bookingForm.Gender,
+                    PhoneNumber = bookingForm.PhoneNumber,
+                    Email = bookingForm.Email,
+                    InsuranceCode = bookingForm.InsuranceCode,
+                    RoleId = 1
+                });
+                if (userRs.IsSuccess)
+                    user = (userRs.Data as User);
+                else throw new Exception(userRs.Message);
+                
+                // get bookingid and bookingorder
+                var bookingRs = await ((BookingRepository)_bookingRepository).GetByIdAsync(bookingForm.BookingId);
+                if (bookingRs.IsSuccess)
+                    booking = (bookingRs.Data as Booking);
+                else throw new Exception(bookingRs.Message);
+
+                // create a insurrance and save it in cookie
+                if (medicalRecordId > 0)
+                {
+                    medicalRecord = await UpdateAMedicalRecord(new MedicalRecord()
+                    {
+                        MedicalRecordId = medicalRecordId,
+                        MedicalDetail = bookingForm.MedicalDetail
+                    });
+                }
+                else
+                {
+                    medicalRecord = await CreateAMedicalRecord(bookingForm, booking, user);
+                    // increase
+                    booking.Order = booking.Order + 1;
+                    bookingRs = await ((BookingRepository)_bookingRepository).UpdateAsync(booking);
+                    if (!bookingRs.IsSuccess)
+                        throw new Exception(bookingRs.Message);
+                }
+               
+                // booking successfull
+                TempData["isSucces"] = true;
+                // set cookie
+                cookieHandler.AddCookie(HttpContext, 1, "MedicalRecordId", medicalRecord.MedicalRecordId.ToString());
+            }
+            catch (Exception ex)
+            {
+                TempData["isSucces"] = false;
+                TempData["ErrMess"] = ex.Message;
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Cancel()
+        {
+            // set cookie
+            var cookieHandler = new CookieService();
+            // declare
+            MedicalRecord medicalRecord = null;
+            var medicalRecordId = Convert.ToInt32(cookieHandler.GetCookie(HttpContext, "MedicalRecordId"));
+            try
+            {
+                if (medicalRecordId <= 0) throw new Exception("medicalRecordId not exist");
+                // get record
+                var medicalRs = await ((MedicalRecordRepository)_medicalRecordRepository).
+                        GetByIdAsync(medicalRecordId);
+                if (medicalRs.IsSuccess)
+                    medicalRecord = (medicalRs.Data as MedicalRecord);
+                else throw new Exception(medicalRs.Message);
+                // update to cancle
+                medicalRecord.Status = ConstVariable.MEDICAL_RECORD_STATUS_CANCEL;
+                medicalRs = await ((MedicalRecordRepository)_medicalRecordRepository).
+                        UpdateAsync(medicalRecord);
+                if (medicalRs.IsSuccess)
+                    medicalRecord = (medicalRs.Data as MedicalRecord);
+                else throw new Exception(medicalRs.Message);
+                // remove
+                cookieHandler.RemoveCookie(HttpContext, "MedicalRecordId");
+
+            }
+            catch (Exception ex)
+            {
+                TempData["isSucces"] = false;
+                TempData["ErrMess"] = ex.Message;
+            }
+            cookieHandler.RemoveCookie(HttpContext, "MedicalRecordId");
             return RedirectToAction("Index");
         }
     }
--- Utils/ConstVariable.cs
+++ Utils/ConstVariable.cs
@@ -2,9 +2,9 @@
 {
     public static class ConstVariable
     {
-        public  const  string MEDICAL_RECORD_STATUS_WAITING = "waiting";
-        public const  string MEDICAL_RECORD_STATUS_SERVED = "served";
+        public const string MEDICAL_RECORD_STATUS_WAITING = "waiting";
+        public const string MEDICAL_RECORD_STATUS_SERVED = "served";
         public const string MEDICAL_RECORD_STATUS_SERVING = "serving";
-        public const string MEDICAL_RECORD_STATUS_CANCLE = "cancle";
+        public const string MEDICAL_RECORD_STATUS_CANCEL = "cancel";
     }
 }
